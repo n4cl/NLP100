f53818a57a26c95d3cc76ebb7ec91dc5af36becc
N4CL <devn4cl@gmail.com>
2016-10-26 01:15:59 +0900

    Add 21.py in ch3

---
+++ ch3/21.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+"""
+21. カテゴリ名を含む行を抽出
+記事中でカテゴリ名を宣言している行を抽出せよ．
+"""
+
+from re import search
+
+def extract_category():
+    row = []
+    with open("uk.txt", "r") as file:
+        for i in file:
+            if search("\[Category:.*\]",i): row.append(i)
+
+    return row
+
+if __name__ == '__main__':
+    text = extract_category()
+    print "".join(text)
