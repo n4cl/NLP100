80996394b7b6eedef7e94b9e4c85de7e9870e2a5
N4CL <devn4cl@gmail.com>
2016-11-02 01:06:22 +0900

    Add 24.py in ch3

---
+++ ch3/24.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+"""
+24. ファイル参照の抽出
+記事から参照されているメディアファイルをすべて抜き出せ．
+"""
+
+from re import search
+
+def extract_media():
+    row = []
+    with open("uk.txt", "r") as file:
+        for i in file:
+            media = search("(File|ファイル):(.*?)\|", i)
+            if media: row.append(media.group(2))
+
+    return row
+
+if __name__ == '__main__':
+    text = extract_media()
+    print "\n".join(text)
