90f70148ea5d670a25115e5a4e427e090fdc72eb
N4CL <devn4cl@gmail.com>
2016-12-24 02:07:49 +0900

    Add ch4_36.py in ch4

---
+++ ch4/ch4_36.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+"""
+36. 単語の出現頻度
+文章中に出現する単語とその出現頻度を求め，出現頻度の高い順に並べよ．
+"""
+
+from ch4_30 import map_morpheme
+from collections import Counter
+
+if __name__ == '__main__':
+
+    with open("neko.txt", "r") as file:
+        text = file.read()
+
+    # 形態素解析処理
+    mapping = map_morpheme(text)
+
+    # 除外リスト
+    surface = ["、", "。", "「", "」", "　"]
+    pos = "接頭詞"
+    pos1 = "接尾"
+
+    words = [word["surface"] for word in mapping \
+             if not word["surface"] in surface \
+             and word["pos"] != pos \
+             and word["pos1"] != pos1]
+
+    counter = Counter(words)
+    for word, cnt in counter.most_common():
+        print word, cnt
