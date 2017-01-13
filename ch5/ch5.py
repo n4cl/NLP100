fa317a37b24f704281cd7be7b9e631ecf8223197
N4CL <devn4cl@gmail.com>
2017-01-14 00:39:58 +0900

    Fix to perform dependency analysis for each sentence

--- ch5/ch5.py
+++ ch5/ch5.py
@@ -8,15 +8,25 @@
 import CaboCha
 
 def analyze_dependency_structure(text):
-
     c = CaboCha.Parser()
     res = c.parse(text)
     return res.toString(CaboCha.FORMAT_LATTICE)
 
 with open("neko.txt", "r") as file:
-    text = file.read()
+    text = file.readline()
+    analysis_result = []
+    i = 0
 
-analysis_result = analyze_dependency_structure(text)
+    while text:
+        text = text.decode("utf-8")
+
+        # 空行は無視
+        if text != "\n":
+            # 字下げ削除
+            if text[0] == u"　": text = text[1:]
+            analysis_result.append(analyze_dependency_structure(text.encode("utf-8")))
+
+        text = file.readline()
 
 with open("neko.txt.cabocha", "w") as file:
-    file.write(analysis_result)
+    file.write("".join(analysis_result))
