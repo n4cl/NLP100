9c562529e7e3af68f84808f436839ef600bbd9d0
N4CL <devn4cl@gmail.com>
2017-01-15 00:03:56 +0900

    Change read method for text

--- ch5/ch5.py
+++ ch5/ch5.py
@@ -13,11 +13,9 @@ def analyze_dependency_structure(text):
     return res.toString(CaboCha.FORMAT_LATTICE)
 
 with open("neko.txt", "r") as file:
-    text = file.readline()
     analysis_result = []
-    i = 0
 
-    while text:
+    for text in file:
         text = text.decode("utf-8")
 
         # 空行は無視
@@ -25,8 +23,6 @@ with open("neko.txt", "r") as file:
             # 字下げ削除
             if text[0] == u"　": text = text[1:]
             analysis_result.append(analyze_dependency_structure(text.encode("utf-8")))
-
-        text = file.readline()
 
 with open("neko.txt.cabocha", "w") as file:
     file.write("".join(analysis_result))
