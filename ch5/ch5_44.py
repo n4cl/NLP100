086c4cdb27748ad3d95f0a1301183e389b735e32
N4CL <devn4cl@gmail.com>
2017-03-10 00:43:32 +0900

    Add ch5_44.py in ch5

---
+++ ch5/ch5_44.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+"""
+44. 係り受け木の可視化
+与えられた文の係り受け木を有向グラフとして可視化せよ．可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．
+また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
+"""
+
+import sys
+import pydot
+from ch5_41 import analyze_dependency_structure
+from ch5_42 import create_relations
+
+# pydotでマルチバイト文字を利用するためにデフォルトエンコーディングを変更
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def create_graph(edges, seq):
+
+    graph = pydot.graph_from_edges(edge_list=edges, directed=True)
+
+    # フォント設定
+    n = pydot.Node("node")
+    n.fontname = "Osaka.ttf"
+    n.fontsize = 9
+    graph.add_node(n)
+
+    # グラフを出力
+    graph.write_png("graph_from_edges_dot_" + str(seq) + ".png", prog="dot")
+
+
+if __name__ == '__main__':
+    r = analyze_dependency_structure("neko.txt.cabocha")
+    relations = create_relations(r)
+    seq = 0
+    for relation in relations:
+        create_graph(relation, seq)
+        seq += 1
