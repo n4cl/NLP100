a640ce4894212f717c387c6ff1c3a43953c21274
N4CL <devn4cl@gmail.com>
2017-03-21 01:58:15 +0900

    Fix valiable name for to limit in scope range

--- ch5/ch5.py
+++ ch5/ch5.py
@@ -8,28 +8,33 @@
 import CaboCha
 
 
-def analyze_dependency_structure(text):
+def analyze_dependency_structure(line):
     """ 係り受け解析を実行する
-    :param text: 行テキスト
+    :param line: 行テキスト
     :return : 行テキストを係り受け解析した結果の文字列
     """
     c = CaboCha.Parser()
-    res = c.parse(text)
+    res = c.parse(line)
     return res.toString(CaboCha.FORMAT_LATTICE)
 
-with open("neko.txt", "r") as file:
-    analysis_result = []
 
-    for text in file:
-        text = text.decode("utf-8")
+def main():
+    with open("neko.txt", "r") as f:
+        analysis_result = []
 
-        # 空行は無視
-        if text != "\n":
-            # 字下げ削除
-            if text[0] == u"　":
-                text = text[1:]
-            analysis_result.append(
-                analyze_dependency_structure(text.encode("utf-8")))
+        for text in f:
+            text = text.decode("utf-8")
 
-with open("neko.txt.cabocha", "w") as file:
-    file.write("".join(analysis_result))
+            # 空行は無視
+            if text != "\n":
+                # 字下げ削除
+                if text[0] == u"　":
+                    text = text[1:]
+                analysis_result.append(
+                    analyze_dependency_structure(text.encode("utf-8")))
+
+    with open("neko.txt.cabocha", "w") as f:
+        f.write("".join(analysis_result))
+
+if __name__ == '__main__':
+    main()
