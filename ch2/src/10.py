bccd75d600cc8402bf4f285715db1ddda51c1ccb
n4cl <devn4cl@gmail.com>
2017-10-07 23:25:03 +0900

    Fix variable name

--- ch2/src/10.py
+++ ch2/src/10.py
@@ -7,9 +7,9 @@
 
 
 def count_line(file_path):
-    with open(file_path, "r") as file:
-        number = sum([1 for line in file])
-        return number
+    with open(file_path, "r") as f:
+        number = sum([1 for _ in f])
+    return number
 
 if __name__ == '__main__':
     print count_line("hightemp.txt")
