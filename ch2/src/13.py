48a64d5546e868f1cc4a2206de74f713230cb91f
n4cl <devn4cl@gmail.com>
2017-10-13 23:49:00 +0900

    Add marge function to 13.py

--- ch2/src/13.py
+++ ch2/src/13.py
@@ -6,16 +6,27 @@
 確認にはpasteコマンドを用いよ．
 """
 
-with open("col1.txt", "r") as col1, open("col2.txt", "r") as col2:
 
-    # テキストを行単位で全て読み込み
-    text1 = col1.readlines()
-    text2 = col2.readlines()
+def marge(text1, text2):
 
     # 一行ごとに結合
-    col_text = [line1.replace("\n", "") + "\t" + line2.replace("\n", "")
-                for line1, line2 in zip(text1, text2)]
-    text = "\n".join(col_text)
+    col_text = [line1.replace("\n", "") + "\t" + line2.replace("\n", "") for line1, line2 in zip(text1, text2)]
+    return "\n".join(col_text)
 
-with open("col1_col2.txt", "w") as f:
-    f.write(text)
+
+def main():
+
+    with open("col1.txt", "r") as col1, open("col2.txt", "r") as col2:
+
+        # テキストを行単位で全て読み込み
+        text1 = col1.readlines()
+        text2 = col2.readlines()
+
+        t = marge(text1, text2)
+
+    with open("col1_col2.txt", "w") as f:
+        f.write(t)
+
+
+if __name__ == '__main__':
+    main()
