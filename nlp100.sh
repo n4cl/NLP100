4f81dd7ae48796720b4c4126b684a858d966e915
n4cl <devn4cl@gmail.com>
2017-12-03 12:07:07 +0900

    Update setup script of ch3 and ch4

--- nlp100.sh
+++ nlp100.sh
@@ -8,9 +8,11 @@ cp -a ./ch2/hightemp.txt ./ch2/src/hightemp.txt
 # ch3
 curl -sS http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz -o ./ch3/jawiki-country.json.gz
 gunzip -c ./ch3/jawiki-country.json.gz > ./ch3/jawiki-country.json
+cp -a ./ch3/jawiki-country.json ./ch3/src/jawiki-country.json
 
 # ch4
 curl -sS http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt -o ./ch4/neko.txt
+cp -a ./ch4/neko.txt ./ch4/src/neko.txt
 
 # ch5
 cp -a ./ch4/neko.txt ./ch5/neko.txt
