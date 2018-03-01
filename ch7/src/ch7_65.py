1ab9e432709f3a34777e9b4a5de9a28c8109ef9f
n4cl <devn4cl@gmail.com>
2018-03-01 23:24:42 +0900

    Add ch7_65.py

---
+++ ch7/src/ch7_65.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+"""
+65. MongoDBの検索
+MongoDBのインタラクティブシェルを用いて，"Queen"というアーティストに関する情報を取得せよ．
+さらに，これと同様の処理を行うプログラムを実装せよ．
+"""
+
+from pymongo import MongoClient
+
+
+def search_artist_info(parm):
+
+    # MongoDBからパラメータより検索
+    client = MongoClient("localhost", 27017)
+    db = client.local
+    collection = db.artist
+
+    return collection.find(parm)
+
+
+def main(parm):
+    artist = search_artist_info(parm)
+
+    for i in artist:
+        print i
+
+if __name__ == '__main__':
+    main({"name": "Queen"})
