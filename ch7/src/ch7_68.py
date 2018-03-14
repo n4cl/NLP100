ffc28f16ecd96409eb4d7687921db64b4539237c
n4cl <devn4cl@gmail.com>
2018-03-15 01:31:06 +0900

    Add ch7_68.py

---
+++ ch7/src/ch7_68.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+"""
+68. ソート
+"dance"というタグを付与されたアーティストの中でレーティングの投票数が多いアーティスト・トップ10を求めよ．
+"""
+
+from pymongo import MongoClient
+
+
+def search_top_artist():
+
+    # MongoDBからパラメータより検索
+    client = MongoClient("localhost", 27017)
+    db = client.local
+    collection = db.artist
+
+    # 問い合わせ内容
+    query = {"tags": {"$elemMatch": {"value": "dance"}}}
+    order_key = "rating.count"
+    order_value = -1
+    top = 10
+
+    return collection.find(query).sort(order_key, order_value).limit(top)
+
+
+def main():
+    artist = search_top_artist()
+
+    for i in artist:
+        print i
+
+if __name__ == '__main__':
+    main()
