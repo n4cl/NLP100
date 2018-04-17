dc8a78f30163c1c858ae31ad905f64e3d3dc28e9
n4cl <devn4cl@gmail.com>
2018-04-17 23:17:03 +0900

    Add rest.js

---
+++ ch7/src/ch7_69/app/static/js/rest.js
@@ -0,0 +1,25 @@
+function syncGetRequest(uri) {
+    "use strict";
+    var req = new XMLHttpRequest();
+    // 同期通信
+    req.open("GET", uri, false);
+    req.send();
+    if (req.status === 200) {
+        // リダイレクトの判定
+        var redURI = req.getResponseHeader('AjaxRedirect');
+        if (redURI) {
+            window.location.href = redURI;
+            return null;
+        } else {
+            if (req.responseText) {
+                return JSON.parse(req.responseText);
+            } else {
+                return null;
+            }
+        }
+    } else if (req.status === 500) {
+        throw new Error('致命的なエラーが発生しました：' + req.status);
+    } else {
+        throw new Error('致命的なエラーが発生しました：' + req.status);
+    }
+}
