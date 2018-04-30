b01db48633cdd160f7be3a29ae2387a65b062736
n4cl <devn4cl@gmail.com>
2018-04-30 19:09:12 +0900

    Add processing to throw request

--- ch7/src/ch7_69/app/static/js/index.js
+++ ch7/src/ch7_69/app/static/js/index.js
@@ -1,27 +1,32 @@
 (function(){
-"use strict";
-let g = {elm: {}};
-g.elm.txt_art_nam = document.getElementById("txt_art_nam");
-g.elm.txt_alt_nam = document.getElementById("txt_alt_nam");
-g.elm.txt_art_tag = document.getElementById("txt_art_tag");
-g.elm.btn_search = document.getElementById("btn_serach");
+    "use strict";
+    var g = {elm: {}};
+    g.elm.txt_art_nam = document.getElementById("txt_art_nam");
+    g.elm.txt_art_als = document.getElementById("txt_art_als");
+    g.elm.txt_art_tag = document.getElementById("txt_art_tag");
+    g.elm.btn_search = document.getElementById("btn_search");
 
-g.row_lst.btn_lst_edit.onclickEx = function (sender) {
-    var row = g.tbl_lst.getRowIndex(sender.element.id);
-    clearDataMas();
-    disabledElement(false);
-    getDataMas(g.tbl_lst.rowWrappers[row].lbl_lst_ord_cod.getValue(), 0, g.val['MNT_MOD']);
+    window.onload = function () {
+        g.elm.txt_art_nam.value = "";
+        g.elm.txt_art_als.value = "";
+        g.elm.txt_art_tag.value = "";
+        g.elm.btn_search.value = "";
+    };
 
-    masDialogVisible(true);
+    g.elm.btn_search.onclick = function () {
+        searchArtist();
+    };
 
-    if(g.row_mas.lbl_mas_mnt_mod_nam.getValue() === "緊急"){
-        g.row_mas.txt_mas_dll_dte.readOnly = true;
-        g.row_mas.sel_mas_jan_kbn.focus();
-    }else if(g.row_mas.lbl_mas_mnt_mod_nam.getValue() === "通常"){
-        g.row_mas.txt_mas_dll_dte.readOnly = false;
-        g.row_mas.txt_mas_dll_dte.focus();
+    // 検索APIを叩くだけ
+    function searchArtist(){
+        var api
+          , parm
+          , uri
+          , json;
+        api = "./search";
+        parm = "name=" + g.elm.txt_art_nam.value + "&alias=" + g.elm.txt_art_als.value + "&tag=" + g.elm.txt_art_tag.value;
+        uri = api + "?" + parm
+        //location.href + api + "?" + parm;
+        json = syncGetRequest(uri);
     }
-
-};
-
-})()
+})();
