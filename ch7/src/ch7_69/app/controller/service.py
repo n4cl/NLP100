cb04744fdc13b91cb268bd9dbc552aa2144cf1fb
n4cl <devn4cl@gmail.com>
2018-05-01 00:22:37 +0900

    Add search api

--- ch7/src/ch7_69/app/controller/service.py
+++ ch7/src/ch7_69/app/controller/service.py
@@ -1,8 +1,12 @@
 # coding: utf-8
+from json import dumps
+from flask import Blueprint, request, render_template, Response
+from app.model.search import ArtistDBClient
 
-from flask import Blueprint, request, render_template
-
-
+"""
+引数であるstatic_folder, template_folderは、
+flaskサーバーを起動したフォルダの位置で適宜変更する
+"""
 app = Blueprint(__name__,
                 "service",
                 url_prefix="",
@@ -20,5 +24,20 @@ def index():
 
 @app.route('/search', methods=["GET"])
 def search():
-    print request.args
-    return "test"
+    name = request.args.get("name")
+    alias = request.args.get("alias")
+    tag = request.args.get("tag")
+
+    client = ArtistDBClient(name=name, alias=alias, tag=tag)
+    client.connect_db()
+    artist = []
+
+    # json化するときに邪魔なので、Object IDを削除する
+    for i in client.fetch_artist():
+        i.pop("_id")
+        artist.append(i)
+
+    res = {"artist": artist}
+
+    # Flaskのjsonifyはlistを扱えないので以下のように返している
+    return Response(dumps(res), mimetype="application/json")
