3e1c23be3e49944a84be5c87fbb5cbce3c4705f7
n4cl <devn4cl@gmail.com>
2018-06-15 21:03:06 +0900

    Add setup.sh for ch8

---
+++ ch8/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eu
+
+curl -sS http://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz -o ./src/rt-polaritydata.tar.gz
+
+tar -zxf ./src/rt-polaritydata.tar.gz -C ./src/
