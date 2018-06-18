eda82c956cdf7368e1578726a77944327206f863
n4cl <devn4cl@gmail.com>
2018-06-18 22:00:21 +0900

    Add character code setting

--- ch8/setup.sh
+++ ch8/setup.sh
@@ -2,5 +2,6 @@
 set -eu
 
 curl -sS http://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz -o ./src/rt-polaritydata.tar.gz
-
 tar -zxf ./src/rt-polaritydata.tar.gz -C ./src/
+nkf -w --overwrite ./src/rt-polaritydata/rt-polarity.pos
+nkf -w --overwrite ./src/rt-polaritydata/rt-polarity.neg
